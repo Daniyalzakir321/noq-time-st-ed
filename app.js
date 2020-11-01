c7e46987e00dcc6ca7f8bfbdab01f3cb713a7873
Daniyal Zakir <41327126+Daniyalzakir321@users.noreply.github.com>
2020-11-01 16:08:02 +0500

    UI WORK- SHAH VIL

---
+++ app.js
@@ -0,0 +1,12 @@
+var noq = document.getElementById("noq").innerHTML
+console.log(noq)
+
+var td = document.getElementById("td").innerHTML
+console.log(td)
+
+
+var sd = document.getElementById("sd").innerHTML
+console.log(sd)
+
+var ed = document.getElementById("ed").innerHTML
+console.log(ed)
